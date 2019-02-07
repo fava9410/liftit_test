cba693934d291fbae2290f7a8e922ff36998bf0f
ticsocial <fredyvargas@ticsocial.com.co>
2019-02-07 12:12:30 -0500

    owner's information in datatable

--- liftit_test/serializer.py
+++ liftit_test/serializer.py
@@ -4,7 +4,10 @@ from rest_framework import serializers
 class VehicleSerializer(serializers.ModelSerializer):
     brand_name = serializers.CharField(source='brand.name', read_only=True)
     type_name = serializers.CharField(source='type.name', read_only=True)
+    #owner = serializers.CharField(source='%s - %s'%('owner.number_document','owner.type_document'),read_only=True)
+    owner_document_type = serializers.CharField(source='owner.type_document.name', read_only=True)
+    owner_document_number = serializers.CharField(source='owner.number_document', read_only=True)
 
     class Meta:
         model = Vehicle
-        fields = ('license_plate','brand_name','type_name')
+        fields = ('license_plate','brand_name','type_name','owner_document_type','owner_document_number')
