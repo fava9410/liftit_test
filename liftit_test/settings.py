46d43df2c8213d08ca887872a557622a08344154
ticsocial <fredyvargas@ticsocial.com.co>
2019-02-07 13:25:14 -0500

    commented lines for heroku's postgresql

--- liftit_test/settings.py
+++ liftit_test/settings.py
@@ -148,3 +148,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL  = '/media/'
+
+#import dj_database_url
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
